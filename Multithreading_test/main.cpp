427bdb3a02ee2ab69561eed9eb4056dda826cb28
lukinalexeyg <lukinalexeyg@gmail.com>
2020-05-16 10:02:26 +0300

    Update main.cpp

--- Multithreading_test/main.cpp
+++ Multithreading_test/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QCoreApplication::setApplicationName(APP_PRODUCT);
+    QCoreApplication::setApplicationVersion(APP_VERSION);
     MainWindow w;
     w.show();
     return a.exec();
